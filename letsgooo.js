c2c64782a12e0c3d01d39d3d1e520cd97df03ff7
abgoat <abdullahhubishan@gmail.com>
2025-09-16 16:55:53 +0300

    letsgooo.js

---
+++ letsgooo.js
@@ -0,0 +1,23 @@
+document.querySelectorAll(".replaceq").forEach(oldScript => {
+  let fixedCode = oldScript.innerHTML
+    .replace(/&quot;/g, '"')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&amp;/g, '&'); // added this line
+
+  // Create a new script element
+  let newScript = document.createElement("script");
+
+  if (oldScript.src) {
+    newScript.src = oldScript.src; // keep external source if present
+  } else {
+    newScript.textContent = fixedCode;
+  }
+
+  // Copy attributes (except src which we handled already)
+  [...oldScript.attributes].forEach(attr => {
+    if (attr.name !== "src") newScript.setAttribute(attr.name, attr.value);
+  });
+
+  oldScript.replaceWith(newScript);
+});
